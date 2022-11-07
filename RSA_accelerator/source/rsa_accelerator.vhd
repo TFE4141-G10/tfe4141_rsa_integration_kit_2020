363e8bc09efcffb73714536454c27316ed835ba9
Kristiss99 <keltius87@gmail.com>
2022-11-07 15:21:55 +0100

    MULTICORE DONE

--- RSA_accelerator/source/rsa_accelerator.vhd
+++ RSA_accelerator/source/rsa_accelerator.vhd
@@ -252,7 +252,7 @@ end generate;
 	
 	core_select : process(msgin_valid_vector, msgin_valid) is
 	begin 
-        if rising_edge(msgin_valid) then
+        if falling_edge(msgin_valid) then
            msgin_valid_vector <= std_logic_vector(shift_left(unsigned(msgin_valid_vector), 1));
         end if;
 	
