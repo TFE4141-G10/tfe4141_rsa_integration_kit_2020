6d798b2f53d17fd0bf2cfdd5706f004861fa8756
espi3000 <espen.holsen.eh@gmail.com>
2022-11-17 16:07:29 +0100

    Documented code

--- Exponentiation/source/exponentiation.vhd
+++ Exponentiation/source/exponentiation.vhd
@@ -1,61 +1,103 @@
+----------------------------------------------------------------------------------
+-- File: exponentiation.vhd
+-- Description: Calculates the modular exponentiation, result = message^key % modulus,
+--              using a left-to-right binary algorithm. 
+--              Used to encrypt and decrypt RSA messages
+-- Create Date: 17/11/2022
+-- Design Name: exponentiation
+-- Module Name: exponentiation
+-- Project Name: RSA_accelerator
+-- Target Devices: PYNC-Z1
+-- Dependencies: modular_multiplication.vhd
+----------------------------------------------------------------------------------
+
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
 entity exponentiation is
 	generic (
-		C_BLOCK_SIZE : integer := 256
+		C_BLOCK_SIZE    : integer := 256
 	);
 	port (
-		--input controll
-		valid_in	: in  std_logic;
-		ready_in	: out std_logic;
-
-		--input data
-		message 	: in  std_logic_vector(C_BLOCK_SIZE - 1 downto 0);
-		key 		: in  std_logic_vector(C_BLOCK_SIZE - 1 downto 0);
-
-		--ouput controll
-		ready_out	: in  std_logic;
-		valid_out	: out std_logic := '0';
-
-		--output data
-		result 		: out std_logic_vector(C_BLOCK_SIZE - 1 downto 0);
-
-		--modulus
-		modulus 	: in  std_logic_vector(C_BLOCK_SIZE - 1 downto 0);
-
-		--utility
-		clk 		: in  std_logic;
-		reset_n 	: in  std_logic;
-
-		--last controll
+		clk 		    : in  std_logic;
+		reset_n 	    : in  std_logic := '1';
+		------------------------------------------------------------------------------
+        -- Controls messages into core
+        ------------------------------------------------------------------------------
+		valid_in	    : in  std_logic;
+		ready_in	    : out std_logic := '1';
+        ------------------------------------------------------------------------------
+        -- Controls results out of core
+        ------------------------------------------------------------------------------
+		ready_out	    : in  std_logic;
+		valid_out	    : out std_logic := '0';
+        ------------------------------------------------------------------------------
+        -- Controls what happens if there is nothing more to calculate
+        ------------------------------------------------------------------------------
 		last_message_in : in std_logic;
-		last_message_out: out std_logic
+		last_result_out : out std_logic := '0';
+		------------------------------------------------------------------------------
+        -- Data input that is used for calculations
+        ------------------------------------------------------------------------------
+		message 	    : in  std_logic_vector(C_BLOCK_SIZE - 1 downto 0);
+		key 		    : in  std_logic_vector(C_BLOCK_SIZE - 1 downto 0);
+        modulus 	    : in  std_logic_vector(C_BLOCK_SIZE - 1 downto 0);
+        ------------------------------------------------------------------------------
+        -- Result that is output from calculation
+        ------------------------------------------------------------------------------
+		result 		    : out std_logic_vector(C_BLOCK_SIZE - 1 downto 0)	
 	);
 end entity;
 
 
 architecture rtl of exponentiation is
-    type   message_state_type        is (uninitialized, idle, load_new_message);
-    signal message_state              : message_state_type                          := uninitialized;
-    signal next_message_state         : message_state_type                          := uninitialized;
-    signal internal_result            : std_logic_vector(C_BLOCK_SIZE - 1 downto 0) := (others => '0');
+    ----------------------------------------------------------------------------------
+    -- Signals related to message state machine
+    ----------------------------------------------------------------------------------
+    type   message_state_type is (uninitialized, idle, load_new_message);
+    signal message_state              : message_state_type := uninitialized;
+    signal next_message_state         : message_state_type := uninitialized;
+
+    ----------------------------------------------------------------------------------
+    -- Inputs and outputs of the modular multiplication core
+    ----------------------------------------------------------------------------------
     signal factor_a                   : std_logic_vector(C_BLOCK_SIZE - 1 downto 0) := (others => '0');
     signal factor_b                   : std_logic_vector(C_BLOCK_SIZE - 1 downto 0) := (others => '0');
     signal start_condition            : std_logic_vector(C_BLOCK_SIZE - 1 downto 0) := (others => '0');
     signal multiplication_result      : std_logic_vector(C_BLOCK_SIZE - 1 downto 0) := (others => '0');
-    signal multiplication_done        : std_logic;
+
+    ----------------------------------------------------------------------------------
+    -- Maps to result. Used for creating register for the result
+    ----------------------------------------------------------------------------------
+    signal internal_result            : std_logic_vector(C_BLOCK_SIZE - 1 downto 0) := (others => '0');
+
+    ----------------------------------------------------------------------------------
+    -- Signals related to the calculations themself
+    ----------------------------------------------------------------------------------
+    signal exponentiation_done        : std_logic := '0';
+    signal multiplication_done        : std_logic := '0';
+    signal clear_multiplication_n     : std_logic := '0';
     signal double_multiplication      : std_logic := '1';
     signal double_multiplication_done : std_logic := '0';
-    signal counter                    : unsigned(7 downto 0) := (others => '1'); 
-    signal internal_message           : std_logic_vector(255 downto 0);
-    signal clear_multiplication_n     : std_logic := '0';
-    signal internal_valid_out         : std_logic := '0';
     signal last_multiplication        : std_logic := '0';
-    signal exponentiation_done        : std_logic := '0';
+
+    ----------------------------------------------------------------------------------
+    -- Used to control the flow of messages in and results out of the core
+    ----------------------------------------------------------------------------------
     signal result_sent_out            : std_logic := '0';
-    signal second_to_last_message_out : std_logic := '0';
+    signal second_to_last_result_out  : std_logic := '0';
+
+    ----------------------------------------------------------------------------------
+    -- Mappings of ports to make readable and writable
+    ----------------------------------------------------------------------------------
+    signal internal_message           : std_logic_vector(255 downto 0);
+    signal internal_valid_out         : std_logic := '0';
+
+    ----------------------------------------------------------------------------------
+    -- Counter used to decide when a calculation is done
+    ----------------------------------------------------------------------------------
+    signal counter                    : unsigned(7 downto 0) := (others => '1'); 
 begin
     ----------------------------------------------------------------------------------
     -- A single multiplication core is used for both multiplication operations in the
@@ -74,11 +116,48 @@ begin
             modulus                  => unsigned(modulus), 
             valid_out                => multiplication_done
         );
-    
+
+    ----------------------------------------------------------------------------------
+    -- These processes decides what values to send to the modular multiplier
+    ----------------------------------------------------------------------------------
+    factor_a        <= start_condition  when counter = C_BLOCK_SIZE - 1       else 
+                       internal_result;
+    factor_b        <= internal_message when double_multiplication_done = '1' else 
+                       factor_a;
+    start_condition <= internal_message when key(C_BLOCK_SIZE - 1) = '1'      else 
+                       std_logic_vector(to_unsigned(1, 256)); 
+
+    ----------------------------------------------------------------------------------
+    -- Desides whether to calculate a single or double multiplication
+    ----------------------------------------------------------------------------------
+    double_multiplication <= '1' when key(to_integer(counter)) = '1' else '0';
+
     ----------------------------------------------------------------------------------
     -- Result changes when internal result changes on the rising edge of the clock
     ----------------------------------------------------------------------------------
     result <= internal_result;
+
+    ----------------------------------------------------------------------------------
+    -- Process for the double multiplication. It has two states:
+    -- 1. Single multiplication: Used when the counter is not at the position where
+    --    a double multiplication is needed
+    -- 2. Double multiplication: Used when the counter is at the position where a double
+    --    multiplication is needed
+    ----------------------------------------------------------------------------------
+    control_multiplication_flow : process(multiplication_done, clk) is
+    begin
+        clear_multiplication_n <= '1';
+        if falling_edge(multiplication_done) then
+            clear_multiplication_n <= '0';
+            internal_result <= multiplication_result;
+            if double_multiplication = '1' and double_multiplication_done = '0' then
+                double_multiplication_done <= '1';
+            else
+                counter <= counter - 1;
+                double_multiplication_done <= '0';
+            end if;
+        end if;
+    end process;
     
     ----------------------------------------------------------------------------------
     -- Valid result when counter wraps around to 255, and can be aquired during the
@@ -88,16 +167,22 @@ begin
     internal_valid_out  <= '1' when exponentiation_done = '1' and result_sent_out = '0' else '0';
     valid_out           <= internal_valid_out;
 
-    check_if_exponentiation_done : process(last_multiplication, result_sent_out, second_to_last_message_out) is
+    ----------------------------------------------------------------------------------
+    -- Checks if the last multiplication is done, and if so, sets the exponentiation_done
+    ----------------------------------------------------------------------------------
+    check_if_exponentiation_done : process(last_multiplication, result_sent_out, second_to_last_result_out) is
     begin
         if result_sent_out = '1' then
             exponentiation_done <= '0';
         elsif falling_edge(last_multiplication) then
             exponentiation_done <= '1';
-            last_message_out    <= second_to_last_message_out;
+            last_result_out     <= second_to_last_result_out;
         end if;
     end process;
 
+    ----------------------------------------------------------------------------------
+    -- Used to reset exponentiation done flag by indicating that the result has been sent
+    ----------------------------------------------------------------------------------
     detect_if_result_sent : process(clk, ready_out, internal_valid_out) is
     begin
         result_sent_out <= '0';
@@ -105,21 +190,6 @@ begin
             result_sent_out <= '1';
         end if;
     end process;
- 
-    ----------------------------------------------------------------------------------
-    -- These processes decides what values to send to the modular multiplier
-    ----------------------------------------------------------------------------------
-    factor_a        <= start_condition when counter = C_BLOCK_SIZE - 1 else
-                       internal_result;
-    factor_b        <= internal_message when double_multiplication_done = '1' else
-                       factor_a;
-    start_condition <= internal_message when key(C_BLOCK_SIZE - 1) = '1' else
-                       std_logic_vector(to_unsigned(1, 256)); 
-
-    ----------------------------------------------------------------------------------
-    -- Desides whether to calculate a single or double multiplication
-    ----------------------------------------------------------------------------------
-    double_multiplication <= '1' when key(to_integer(counter)) = '1' else '0';
 
     ----------------------------------------------------------------------------------
     -- Changes message state every clock cycle
@@ -141,17 +211,16 @@ begin
     --    a new message
     -- 3. Load new message: Used when the core is ready to accept a new message
     ----------------------------------------------------------------------------------
-    message_state_machine : process(message_state, valid_in, second_to_last_message_out, internal_valid_out, ready_out) is
+    message_state_machine : process(message_state, valid_in, second_to_last_result_out, internal_valid_out, ready_out) is
     begin
         case message_state is
             when uninitialized =>
-                if second_to_last_message_out = '0' then
+                if second_to_last_result_out = '0' then
                     ready_in <= '1';
                 else
                     ready_in <= '0';
                 end if;
                 if valid_in = '1' then
-                    --internal_message   <= message;
                     next_message_state <= idle;
                 else
                     next_message_state <= uninitialized;
@@ -177,7 +246,8 @@ begin
     end process;
 
     ----------------------------------------------------------------------------------
-    -- 
+    -- Overwrites internal message register with new message when the core is ready 
+    -- to accept a new message
     ----------------------------------------------------------------------------------
     acquire_new_message : process(clk, message_state, valid_in, message) is
     begin
@@ -187,34 +257,12 @@ begin
     end process;
 
     ----------------------------------------------------------------------------------
-    -- Sets last message out signal high when the last message has been received
+    -- Sets second to last out signal so that it is ready to set the last out signal
     ----------------------------------------------------------------------------------
-    set_last_message_out : process(clk, message_state, valid_in, last_message_in) is
+    set_second_to_last_result_out : process(clk, message_state, valid_in, last_message_in) is
     begin
         if rising_edge(clk) and message_state = uninitialized and valid_in = '1' then
-            second_to_last_message_out <= last_message_in;
-        end if;
-    end process;
-
-    ----------------------------------------------------------------------------------
-    -- FSM for the double multiplication. It has two states:
-    -- 1. Single multiplication: Used when the counter is not at the position where
-    --    a double multiplication is needed
-    -- 2. Double multiplication: Used when the counter is at the position where a double
-    --    multiplication is needed
-    ----------------------------------------------------------------------------------
-    process(multiplication_done, clk) is
-    begin
-        clear_multiplication_n <= '1';
-        if falling_edge(multiplication_done) then
-            clear_multiplication_n <= '0';
-            internal_result <= multiplication_result;
-            if double_multiplication = '1' and double_multiplication_done = '0' then
-                double_multiplication_done <= '1';
-            else
-                counter <= counter - 1;
-                double_multiplication_done <= '0';
-            end if;
+            second_to_last_result_out <= last_message_in;
         end if;
     end process;
 end architecture;
