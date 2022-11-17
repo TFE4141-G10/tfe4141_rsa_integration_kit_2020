c4c1ed90eb6478cd3536591a467f9a0cd54c5b32
Kristiss99 <keltius87@gmail.com>
2022-11-17 14:59:25 +0100

    Working multicore design

--- RSA_accelerator/source/rsa_core.vhd
+++ RSA_accelerator/source/rsa_core.vhd
@@ -77,19 +77,18 @@ architecture rtl of rsa_core is
 	signal msgin_valid_vector: std_logic_vector(N downto 0) := (others => '0');
 	signal msgout_valid_vector:std_logic_vector(N downto 0) := (others => '0');
 	signal msgin_ready_vector: std_logic_vector(N downto 0) := (others => '0');
-    signal msgout_ready_vector: std_logic_vector(N downto 0) := (0 => '1', others => '0');
+    signal msgout_ready_vector: std_logic_vector(N downto 0) := (others => '0');
     signal msgout_last_vector:std_logic_vector(N downto 0) := (others => '0');
+    signal msgin_last_vector: std_logic_vector(N downto 0):= (others => '0');
     signal msgin_select_vector: std_logic_vector(N downto 0) := (0 => '1', others => '0');
+    signal msgout_select_vector: std_logic_vector(N downto 0) := (0 => '1', others => '0');
+    
     
     type data_array is array(N downto 0) of std_logic_vector(255 downto 0); 
     signal msgout_data_array : data_array;
     type status_array is array(N downto 0) of std_logic_vector(31 downto 0); 
-    signal rsa_status_array : status_array;
-    signal rsa_status_0: std_logic_vector(31 downto 0); 
-    signal rsa_status_1: std_logic_vector(31 downto 0); 
-    signal msgout_signal_0: std_logic_vector(255 downto 0); 
-    signal msgout_signal_1: std_logic_vector(255 downto 0); 
-    signal msgin_last_vector: std_logic_vector(255 downto 0);
+    signal rsa_status_array : status_array; 
+
     
     
 begin
@@ -149,6 +148,16 @@ port map (
     --rsa_status             => rsa_status_array(i)
 
     );
+    -----------------------------------------------------------------------------
+	-- Multicore signal to vector handeling
+	-----------------------------------------------------------------------------
+    
+    msgin_valid_vector(i) <= msgin_valid when msgin_select_vector(i) = '1' else
+	'0';
+	msgout_ready_vector(i) <= msgout_ready when msgout_select_vector(i) = '1' else
+	'0';
+	msgin_last_vector(i) <= msgin_last when msgin_select_vector(i) = '1' else
+	'0';
 
 end generate;
 	
@@ -156,44 +165,27 @@ end generate;
 	-- Multicore signal handeling
 	-----------------------------------------------------------------------------
 	msgout_data <= msgout_data_array(0) when msgout_ready_vector(0) = '1' else
-	
---	generate_data_array : for i in 0 to N generate
---		msgout_data <= msgout_data_array(i) when msgout_ready_vector(i) = '1';
---	end generate;
+	msgout_data_array(1) when msgout_ready_vector(1) = '1' else
+	(others => '0');
 
-	msgout_data_array(1) when msgout_ready_vector(1) = '1';
-	-- msgout_data_array(2) when msgout_ready_vector(2) = '1' else
-	-- msgout_data_array(3) when msgout_ready_vector(3) = '1' else
-	-- msgout_data_array(4) when msgout_ready_vector(4) = '1' else
-	-- msgout_data_array(N) when msgout_ready_vector(N) = '1';
-	
-	rsa_status <= rsa_status_array(0) when msgout_ready_vector(0) = '1' else
-	rsa_status_array(1) when msgout_ready_vector(1) = '1'; --else
-	--rsa_status_array(2) when msgout_ready_vector(2) = '1'; --else
---	rsa_status_array(3) when msgout_ready_vector(3) = '1' else
---	rsa_status_array(4) when msgout_ready_vector(4) = '1' else
---	rsa_status_array(5) when msgout_ready_vector(5) = '1';
+	rsa_status <= rsa_status_array(0) when msgout_select_vector(0) = '1' else
+	rsa_status_array(1) when msgout_select_vector(1) = '1' else
+	(others => '0'); 
 
 	
-	msgout_last <= msgout_last_vector(0) when msgout_ready_vector(0) = '1' else
-	msgout_last_vector(1) when msgout_ready_vector(1) = '1';
-	
-	msgout_valid <= msgout_valid_vector(0) when msgout_ready_vector(0) = '1' else
-	msgout_valid_vector(1) when msgout_ready_vector(1) = '1';
-	
-	--msgin_valid <= msgin_valid_vector(0) when msgout_ready_vector(0) = '1' else
-	--msgin_valid_vector(1) when msgout_ready_vector(1) = '1';
-	
-	msgin_ready <= msgin_ready_vector(0); --when msgin_valid_vector(0) = '1' else
-	--msgin_ready_vector(1) when msgin_valid_vector(1) = '1';
+	msgout_last <= msgout_last_vector(0) when msgout_select_vector(0) = '1' else
+	msgout_last_vector(1) when msgout_select_vector(1) = '1' else
+	'0';
 	
 	
+	msgin_ready <= msgin_ready_vector(0) when msgin_select_vector(0) = '1' else
+	msgin_ready_vector(1) when msgin_select_vector(1) = '1' else
+	'0';
 	
 	
-	msgin_valid_vector(0) <= msgin_valid when msgin_select_vector(0) = '1';
-	msgin_valid_vector(1) <= msgin_valid  when msgin_select_vector(1) = '1';
-	
-	
+	msgout_valid <= msgout_valid_vector(0) when msgout_select_vector(0) = '1' else
+	msgout_valid_vector(1) when msgout_select_vector(1) = '1' else
+	'0';	
 	
 	
 	
@@ -202,30 +194,31 @@ end generate;
 	-- Multicore logic handeling
 	-----------------------------------------------------------------------------
 	
-	core_select : process(clk, msgin_select_vector, msgin_valid, msgin_last) is
+	input_select : process(clk, msgin_select_vector, msgin_valid, msgin_last) is
 	begin 
         if rising_edge(clk) then
             if msgin_valid = '1' and msgin_last = '0' then
+                if msgin_select_vector(N) = '1' then
+                  msgin_select_vector <= (0 => '1', others => '0');
+                else
                 msgin_select_vector <= std_logic_vector(shift_left(unsigned(msgin_select_vector), 1));
+                end if;
             end if;
         end if;
 	end process;
 	
-
---	last_vector: process(msgin_last) is 
---	   begin
---	   if msgin_last = '1' then
---	      msgin_last_vector <= msgin_valid_vector;
---       end if;
---       end process;
 	
-	output_select : process(msgout_ready_vector, msgout_ready, msgout_last) is
+	output_select : process(clk, msgout_select_vector, msgout_ready, msgout_valid) is
 	begin
-	if rising_edge(clk) then
-        if msgout_ready = '1' and msgout_last = '0' then 
-           msgout_ready_vector <= std_logic_vector(shift_left(unsigned(msgout_ready_vector), 1));
+        if rising_edge(clk) then
+            if msgout_ready = '1' and msgout_valid = '1' and msgout_last = '0' then
+               if msgout_select_vector(N) = '1' then
+                  msgout_select_vector <= (0 => '1', others => '0');
+               else
+                  msgout_select_vector <= std_logic_vector(shift_left(unsigned(msgout_select_vector), 1));
+               end if;
+            end if;
         end if;
-    end if;
 	end process;
 	
 
