a6c3f2bfe6fc5f2ac57047d51c4e8cb664be044c
espi3000 <espen.holsen.eh@gmail.com>
2022-11-18 12:45:53 +0100

    Added clean accelerator

--- RSA_accelerator/source/rsa_accelerator.vhd
+++ RSA_accelerator/source/rsa_accelerator.vhd
@@ -17,7 +17,6 @@ entity rsa_accelerator is
 	generic (
 		-- Users to add parameters here
 		C_BLOCK_SIZE : integer := 256;
-		N : integer := 1;
 
 		-- User parameters ends
 		
@@ -107,25 +106,6 @@ architecture rtl of rsa_accelerator is
 	signal key_n        : std_logic_vector(C_BLOCK_SIZE-1 downto 0);
 	signal rsa_status   : std_logic_vector(31 downto 0);
 
-	-----------------------------------------------------------------------------
-	-- Signals for multicore design
-	-----------------------------------------------------------------------------
-	signal msgin_valid_vector: std_logic_vector(N downto 0) := (0 => '1', others => '0');
-	signal msgout_valid_vector:std_logic_vector(N downto 0) := (others => '0');
-	signal msgin_ready_vector: std_logic_vector(N downto 0) := (others => '0');
-    signal msgout_ready_vector: std_logic_vector(N downto 0) := (0 => '1', others => '0');
-    signal msgout_last_vector:std_logic_vector(N downto 0) := (others => '0');
-    
-    type data_array is array(N downto 0) of std_logic_vector(255 downto 0); 
-    signal msgout_data_array : data_array;
-    type status_array is array(N downto 0) of std_logic_vector(31 downto 0); 
-    signal rsa_status_array : status_array;
-    signal rsa_status_0: std_logic_vector(31 downto 0); 
-    signal rsa_status_1: std_logic_vector(31 downto 0); 
-    signal msgout_signal_0: std_logic_vector(255 downto 0); 
-    signal msgout_signal_1: std_logic_vector(255 downto 0); 
-    signal msgin_last_vector: std_logic_vector(255 downto 0);
-    
 begin
 
 -- Instantiation of Axi Bus Interface S00_AXI
@@ -211,154 +191,40 @@ u_rsa_msgout : entity work.rsa_msgout
 		msgout_last            => msgout_last
 	);
 
+u_rsa_core : entity work.rsa_core
+	generic map (
+		C_BLOCK_SIZE => C_BLOCK_SIZE
+	)
+	port map (
+		-----------------------------------------------------------------------------
+		-- Clocks and reset
+		-----------------------------------------------------------------------------
+		clk                    => clk,
+		reset_n                => reset_n,
 
+		-----------------------------------------------------------------------------
+		-- Slave msgin interface
+		-----------------------------------------------------------------------------
+		msgin_valid            => msgin_valid,
+		msgin_ready            => msgin_ready,
+		msgin_data             => msgin_data,
+		msgin_last             => msgin_last,
 
-	-----------------------------------------------------------------------------
-	-- Generation of mulicore interface
-	-----------------------------------------------------------------------------
---generate_cores : for i in 0 to N generate
- u_rsa_core : entity work.rsa_core
- 	generic map (
- 		C_BLOCK_SIZE => C_BLOCK_SIZE
- 	)
- 	port map (
- 		-----------------------------------------------------------------------------
- 		-- Clocks and reset
- 		-----------------------------------------------------------------------------
- 		clk                    => clk,
- 		reset_n                => reset_n,
+		-----------------------------------------------------------------------------
+		-- Master msgout interface
+		-----------------------------------------------------------------------------
+		msgout_valid           => msgout_valid,
+		msgout_ready           => msgout_ready,
+		msgout_data            => msgout_data,
+		msgout_last            => msgout_last,
 
- 		-----------------------------------------------------------------------------
- 		-- Slave msgin interface
- 		-----------------------------------------------------------------------------
- 		msgin_valid            => msgin_valid,
- 		msgin_ready            => msgin_ready,
- 		msgin_data             => msgin_data,
- 		msgin_last             => msgin_last,
+		-----------------------------------------------------------------------------
+		-- Interface to the register block
+		-----------------------------------------------------------------------------
+		key_e_d                => key_e_d,
+		key_n                  => key_n,
+		rsa_status             => rsa_status
 
- 		-----------------------------------------------------------------------------
- 		-- Master msgout interface
- 		-----------------------------------------------------------------------------
- 		msgout_valid           => msgout_valid,
- 		msgout_ready           => msgout_ready,
- 		msgout_data            => msgout_data,
- 		msgout_last            => msgout_last,
-
- 		-----------------------------------------------------------------------------
- 		-- Interface to the register block
- 		-----------------------------------------------------------------------------
- 		key_e_d                => key_e_d,
- 		key_n                  => key_n,
- 		rsa_status             => rsa_status
-
-	 	);
-	
---u_rsa_core : entity work.rsa_core
---generic map (
---    C_BLOCK_SIZE => C_BLOCK_SIZE
---)
---port map (
---    -----------------------------------------------------------------------------
---    -- Clocks and reset
---    -----------------------------------------------------------------------------
---    clk                    => clk,
---    reset_n                => reset_n,
-
---    -----------------------------------------------------------------------------
---    -- Slave msgin interface
---    -----------------------------------------------------------------------------
---    msgin_valid            => msgin_valid_vector(i),
---    msgin_ready            => msgin_ready_vector(i),
---    msgin_data             => msgin_data,
---    msgin_last             => msgin_last_vector(i),
-
---    -----------------------------------------------------------------------------
---    -- Master msgout interface
---    -----------------------------------------------------------------------------
---    msgout_valid           => msgout_valid_vector(i),
---    msgout_ready           => msgout_ready_vector(i),
---    msgout_data            => msgout_data_array(i),
---    msgout_last            => msgout_last_vector(i),
-
---    -----------------------------------------------------------------------------
---    -- Interface to the register block
---    -----------------------------------------------------------------------------
---    key_e_d                => key_e_d,
---    key_n                  => key_n,
---    rsa_status             => rsa_status_array(i)
-
---    );
-
---end generate;
-	
---	-----------------------------------------------------------------------------
---	-- Multicore signal handeling
---	-----------------------------------------------------------------------------
---	msgout_data <= msgout_data_array(0) when msgout_ready_vector(0) = '1' else
-	
-----	generate_data_array : for i in 0 to N generate
-----		msgout_data <= msgout_data_array(i) when msgout_ready_vector(i) = '1';
-----	end generate;
-
---	msgout_data_array(1) when msgout_ready_vector(1) = '1';
---	-- msgout_data_array(2) when msgout_ready_vector(2) = '1' else
---	-- msgout_data_array(3) when msgout_ready_vector(3) = '1' else
---	-- msgout_data_array(4) when msgout_ready_vector(4) = '1' else
---	-- msgout_data_array(N) when msgout_ready_vector(N) = '1';
-	
---	rsa_status <= rsa_status_array(0) when msgout_ready_vector(0) = '1' else
---	rsa_status_array(1) when msgout_ready_vector(1) = '1'; --else
---	--rsa_status_array(2) when msgout_ready_vector(2) = '1'; --else
-----	rsa_status_array(3) when msgout_ready_vector(3) = '1' else
-----	rsa_status_array(4) when msgout_ready_vector(4) = '1' else
-----	rsa_status_array(5) when msgout_ready_vector(5) = '1';
-
-	
---	msgout_last <= msgout_last_vector(0) when msgout_ready_vector(0) = '1' else
---	msgout_last_vector(1) when msgout_ready_vector(1) = '1';
-	
---	--msgin_valid <= msgin_valid_vector(0) when msgout_ready_vector(0) = '1' else
---	--msgin_valid_vector(1) when msgout_ready_vector(1) = '1';
-	
---	msgin_ready <= msgin_ready_vector(0) when msgin_valid_vector(0) = '1' else
---	msgin_ready_vector(1) when msgin_valid_vector(1) = '1';
-	
-	
-	
-	
---	-- msgin_valid <= msgin_valid_vector(0) when msgin_valid_vector(0) = '1' else
---	-- msgin_valid_vector(1) when msgin_valid_vector(1) = '1';
-	
-	
-	
-	
-	
-	
---	-----------------------------------------------------------------------------
---	-- Multicore logic handeling
---	-----------------------------------------------------------------------------
-	
---	core_select : process(msgin_valid_vector, msgin_ready, msgin_last) is
---	begin 
---        if msgin_ready = '1' and msgin_last = '0' then
---            msgin_valid_vector <= std_logic_vector(shift_left(unsigned(msgin_valid_vector), 1));
---        end if;
---	end process;
-	
-
---	last_vector: process(msgin_last) is 
---	   begin
---	   if msgin_last = '1' then
---	      msgin_last_vector <= msgin_valid_vector;
---       end if;
---       end process;
-	
---	output_select : process(msgout_ready_vector, msgout_ready, msgout_last) is
---	begin
---	if msgout_ready = '1' and msgout_last = '0' then 
---	   msgout_ready_vector <= std_logic_vector(shift_left(unsigned(msgout_ready_vector), 1));
---	end if;
---	end process;
-	
+	);
 
 end rtl;
