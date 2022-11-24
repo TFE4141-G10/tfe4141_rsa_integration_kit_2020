ae68a69aa787852fcc806520d1a3ad5a13d4f6f7
espi3000 <espen.holsen.eh@gmail.com>
2022-11-24 10:24:12 +0100

    Temp

--- Exponentiation/testbench/modular_multiplication_tb.vhd
+++ Exponentiation/testbench/modular_multiplication_tb.vhd
@@ -26,8 +26,9 @@ entity modular_multiplication_tb is
 end entity;
 
 architecture behavior of modular_multiplication_tb is
-    constant T            : time      := 20 ps; 
+    constant T            : time      := 10 ns; 
     constant C_BLOCK_SIZE : natural   := 256;
+
     signal   clk          : std_logic := '0';
     signal   reset_n      : std_logic := '0';
     signal   valid_out    : std_logic;
@@ -36,9 +37,31 @@ architecture behavior of modular_multiplication_tb is
     signal   modulus      : unsigned(C_BLOCK_SIZE - 1 downto 0);
     signal   result       : unsigned(C_BLOCK_SIZE - 1 downto 0);
     signal   result_out   : unsigned(C_BLOCK_SIZE - 1 downto 0);
-begin
-    clk <= not clk after T/2;
 
+    file     f_inputs     : text open read_mode is "inputs.txt";
+
+    procedure check_result (
+        result   : unsigned(C_BLOCK_SIZE - 1 downto 0); 
+        factor_a : unsigned(C_BLOCK_SIZE - 1 downto 0); 
+        factor_b : unsigned(C_BLOCK_SIZE - 1 downto 0); 
+        modulus  : unsigned(C_BLOCK_SIZE - 1 downto 0)
+    ) is
+        variable expected_result : unsigned(C_BLOCK_SIZE - 1 downto 0);
+    begin
+        report LF & LF &
+               "Checking (a*b)%n"                  & LF &
+               "a: " & "0x" & to_hstring(factor_a) & LF &
+               "b: " & "0x" & to_hstring(factor_b) & LF &
+               "n: " & "0x" & to_hstring(modulus);
+
+        expected_result := (factor_a*factor_b) mod modulus;
+        assert result = expected_result 
+            report "Result differs from the expected result"         & LF &
+                   "Expected: " & "0x" & to_hstring(expected_result) & LF &
+                   "Actual:   " & "0x" & to_hstring(result)
+            severity error;
+    end procedure;
+begin
     DUT : entity work.modular_multiplication
         generic map (
             C_BLOCK_SIZE => C_BLOCK_SIZE
@@ -53,16 +76,25 @@ begin
             result       => result
         );
 
-        result_process : process(valid_out, clk) is
-        begin
+    clk <= not clk after T/2;
+
+    get_result : process(clk) is
+    begin
+        if rising_edge(clk) then
             reset_n <= '1';
-            if rising_edge(clk) and valid_out = '1' then
+            if valid_out = '1' then
                 reset_n    <= '0';
                 result_out <= result;
             end if;
-        end process;
+        end if;
+    end process;
 
-    process
+    process is
+    begin
+        
+    end process;
+
+    set_stimuli : process is
     begin
         -- factor_a <= 0x"85ee722363960779206a2b37cc8b64b5fc12a934473fa0204bbaaf714bc90c01";
         -- factor_b <= 0x"08f9baf32e8505cbc9a28fed4d5791dce46508c3d1636232bf91f5d0b6632a9f";
@@ -74,7 +106,9 @@ begin
 
         wait until valid_out = '1';
         wait until valid_out = '0';
+        check_result(result_out, factor_a, factor_b, modulus);
 
+        file_close(f_inputs);
         wait;
     end process;
 end architecture;
